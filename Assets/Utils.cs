98966ee7a37d944a736fe3861a323a838846d7ea
Unknown <ncbiller@gmail.com>
2017-12-29 12:22:16 +0000

    Adding Perlin Noise

---
+++ Assets/Utils.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils {
+
+    static int maxHeight = 150;
+    static float smooth = 0.01f;
+    static int octaves = 4;
+    static float persistence = 0.5f;
+
+    public static int GenerateHeight(float x, float z)
+    {
+        float height = Map(0, maxHeight, 0, 1, fBM(x * smooth, z * smooth, octaves, persistence));
+        return (int)height;
+    }
+
+
+
+
+    static float Map(float min, float max, float omin, float omax, float value)
+    {
+        return Mathf.Lerp(min, max, Mathf.InverseLerp(omin, omax, value));
+    }
+
+    static float fBM(float x, float z, int octaves, float persistence)
+    {
+        float total = 0;
+        float frequency = 1;
+        float amplitude = 1;
+        float maxValue = 0;
+        for (int i = 0; i < octaves; i++)
+        {
+            total += Mathf.PerlinNoise(x * frequency, z * frequency) * amplitude;
+            maxValue += amplitude;
+            amplitude *= persistence;
+            frequency *= 2;
+        }
+
+        return total / maxValue;
+    }
+}
--- Assets/World.cs
+++ Assets/World.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class World : MonoBehaviour {
     public Material textureAtlas;
 
-    public static int columnHeight = 2;
+    public static int columnHeight = 16;
     public static int chunkSize = 16;
-    public static int worldSize = 2;
+    public static int worldSize = 8;
     public static Dictionary<string, Chunk> chunks;
    
 
